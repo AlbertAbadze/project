2858666683f7b8051bfae6fb3b27c4c1a9949ba9
Albert <abadze01@yandex.ru>
2022-08-09 12:50:49 +0300

    add code Math

---
+++ poject/Example007_code/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число");
+int numberA = int.Parse(Console.ReadLine());
+
+int sqr = numberA * numberA;
+Console.WriteLine(sqr);
+sqr = Convert.ToInt32(Math.Pow(numberA, 2));
+Console.WriteLine(sqr);
