235e9287da101da15c05296dfef3e0f7171c8ba6
Albert <abadze01@yandex.ru>
2022-08-09 14:39:17 +0300

    MaxAndMin

---
+++ poject/Example009_MaxAndMin/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int a = 7;
+int b = 15;
+int max = 0;
+int min = 0;
+
+if(a>b)
+{
+    max = a;
+    min = b;
+}
+else 
+{
+    max = b;
+    min = a;
+}  
+
+Console.WriteLine($"Max{max} Min{min}");
