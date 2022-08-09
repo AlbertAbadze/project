f7641442a0eb3e1a009b8495b49a956a1670625f
Albert <abadze01@yandex.ru>
2022-08-09 14:07:23 +0300

    while

---
+++ poject/Example009_while/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int count = 0;
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+count = count - (number + 1);
+
+while (count < number)  
+{
+    count += 1;
+    Console.WriteLine(count + " ");
+}
